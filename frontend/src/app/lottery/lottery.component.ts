46e3aeb816d3e9bb6b93715dd17c6a8e8f74cf92
Marco <marco@mrgt.me>
2022-10-22 13:53:54 +0100

    added ts contract as back to copy from

--- frontend/src/app/lottery/lottery.component.ts
+++ frontend/src/app/lottery/lottery.component.ts
@@ -1,33 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { ethers } from 'ethers';
 import { WalletService } from '../services/wallet.service';
+import { Lottery, LotteryToken } from '../../assets/abi';
 
-const LOTTERY_ADDRESS = '0x109565dFdA78De21A7E8459fb008B108f0d9A09b'; 
+const LOTTERY_ADDRESS = '0x109565dFdA78De21A7E8459fb008B108f0d9A09b';
 const LOTTERYTOKEN_ADDRESS = '0xFa0A807a048D56E5b2Bdd59b570dBe4834bf802e';
+
 @Component({
   selector: 'app-lottery',
   templateUrl: './lottery.component.html',
-  styleUrls: ['./lottery.component.scss']
+  styleUrls: ['./lottery.component.scss'],
 })
 export class LotteryComponent implements OnInit {
   walletId: string = '';
   provider: any;
   signer: any;
+  contract: any;
+  token: any;
   public ethereum;
 
-  constructor(private walletService: WalletService) { 
+  constructor(private walletService: WalletService) {
     this.ethereum = (window as any).ethereum;
     this.provider = new ethers.providers.Web3Provider(this.ethereum);
     this.signer = this.provider.getSigner();
+    this.contract = Lottery;
+    this.token = LotteryToken;
   }
 
   ngOnInit(): void {
     this.walletService
-    .checkWalletConnected()
-    .then((accounts) => (this.walletId = accounts[0]));
+      .checkWalletConnected()
+      .then((accounts) => (this.walletId = accounts[0]));
   }
 
   async getLotteryState() {
     console.log('Getting lottery state...');
+    alert('Getting lottery state...');
+  }
+
+  async checkState() {
+    const state = await this.contract.betsOpen();
+    console.log(`The lottery is ${state ? 'open' : 'closed'}\n`);
+    if (!state) return;
+    this.provider
+      .getBlock('latest')
+      .then((currentBlock: any) => {
+        const currentBlockDate = new Date(currentBlock.timestamp * 1000);
+        console.log(
+          `The last block was mined at ${currentBlockDate.toLocaleDateString()} : ${currentBlockDate.toLocaleTimeString()}\n`
+        );
+      })
+      .catch((error: any) => {
+        console.log('error\n');
+        alert({ error });
+      });
+    const closingTime = await this.contract.betsClosingTime();
+    const closingTimeDate = new Date(closingTime.toNumber() * 1000);
+    console.log(
+      `lottery should close at  ${closingTimeDate.toLocaleDateString()} : ${closingTimeDate.toLocaleTimeString()}\n`
+    );
+  }
+
+  async openBets(duration: string) {
+    console.log('Opening bets...');
+    alert('Opening bets...');
   }
 }
