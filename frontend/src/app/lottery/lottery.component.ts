b7909764cd8de1242e80da69e8f21232e4cd6348
Marco <marco@mrgt.me>
2022-10-23 14:16:52 +0100

    added buttons for all functions

--- frontend/src/app/lottery/lottery.component.ts
+++ frontend/src/app/lottery/lottery.component.ts
@@ -69,11 +69,12 @@ export class LotteryComponent implements OnInit {
 
   _duration = new FormControl(0);
   async openBet(duration: any) {
-    alert(duration);
     const currentBlock = await this.provider.getBlock('latest');
-    const tx = await this.contract.openBets(
-      currentBlock.timestamp + Number(duration)
-    );
+    const tx = await this.contract
+      .openBets(currentBlock.timestamp + Number(duration))
+      .catch((error: any) => {
+        alert(error.message);
+      });
     const receipt = await tx.wait();
     console.log(`Bets opened (${receipt.transactionHash})`);
     const openBets = await this.contract.openBets(duration);
@@ -93,27 +94,29 @@ export class LotteryComponent implements OnInit {
     alert(`The account of address ${this.walletId} has ${balance} Tokens\n`);
   }
 
-  // this one doesn't work yet
   _betAmount = new FormControl(0);
   async bet(amount: any) {
     const allowTx = await this.token
-      .connect(this.walletId)
-      .approve(this.contract.address, ethers.constants.MaxUint256);
+      .connect(this.signer)
+      .approve(this.contract.address, ethers.constants.MaxUint256)
+      .catch((error: any) => {
+        alert(error.message);
+      });
     await allowTx.wait();
     const tx = await this.contract.connect(this.walletId).betMany(amount);
     const receipt = await tx.wait();
     alert(`Bets placed (${receipt.transactionHash})\n`);
   }
 
-  // connect missing on all the ones below this:
-
   async closeLottery() {
-    const tx = await this.contract.closeLottery();
+    const tx = await this.contract.closeLottery().catch((error: any) => {
+      alert(error.message);
+    });
     const receipt = await tx.wait();
     alert(`Bets closed (${receipt.transactionHash})\n`);
   }
 
-  async displayPrize(index: string): Promise<string> {
+  async displayPrize(): Promise<string> {
     const prizeBN = await this.contract.prize(this.walletId);
     const prize = ethers.utils.formatEther(prizeBN);
     alert(
@@ -123,28 +126,39 @@ export class LotteryComponent implements OnInit {
     return prize;
   }
 
-  async claimPrize(index: string, amount: string) {
+  async claimPrize() {
+    const _amount = await this.displayPrize();
     const tx = await this.contract
-      .connect(this.walletId)
-      .prizeWithdraw(ethers.utils.parseEther(amount));
+      .connect(this.signer)
+      .prizeWithdraw(ethers.utils.parseEther(_amount))
+      .catch((error: any) => {
+        alert(error.message);
+      });
     const receipt = await tx.wait();
     console.log(`Prize claimed (${receipt.transactionHash})\n`);
   }
 
+  // this one is not working
   async displayOwnerPool() {
     const balanceBN = await this.contract.ownerPool();
     const balance = ethers.utils.formatEther(balanceBN);
     console.log(`The owner pool has (${balance}) Tokens \n`);
+    return balance;
   }
 
-  async withdrawTokens(amount: any) {
-    const tx = await this.contract.ownerWithdraw(
-      ethers.utils.parseEther(amount)
-    );
+  //this one works
+  async drainOwnerPool() {
+    const poolBalance = await this.displayOwnerPool();
+    const tx = await this.contract
+      .ownerWithdraw(ethers.utils.parseEther(poolBalance))
+      .catch((error: any) => {
+        alert(error.message);
+      });
     const receipt = await tx.wait();
     console.log(`Withdraw confirmed (${receipt.transactionHash})\n`);
   }
 
+  _burnAmount = new FormControl(0);
   async burnTokens(amount: any) {
     const allowTx = await this.token
       .connect(this.walletId)
